df9d7c4b0ce0aad6ef125b19bc8ee8e1436da106
adicasa <69724649+adicasa@users.noreply.github.com>
2020-08-23 01:20:39 -0400

    segundo envio incluyendo dos archivos

---
+++ aplic.js
@@ -0,0 +1,15 @@
+
+<html>
+<body>
+<h2>My Heading</h2>
+<p>Play around with the code and click on the "Run" button to view the result.</p>
+<p id="demo"></p>
+<script>
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML =
+"The value of z is: " + z;
+</script>
+</body>
+</html>
